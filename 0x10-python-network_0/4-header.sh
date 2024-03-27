38a0df2fc2a2110237385f40f8ca55cbc22de0d7
mershark <mersharktakyi@gmail.com>
2024-03-27 21:18:17 +0000

    correct

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
+response=$(curl -s -i "$1" -X GET -H "X-HolbertonSchool-User-Id: 98")
+echo "$response"
